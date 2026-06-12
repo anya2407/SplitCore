36ee5341fc34d2c82b4c046c2f9850c184a84b5b
Anya Arora <anyaarora2407@gmail.com>
2026-06-12 14:33:48 +0530

    final

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:%40C82A221S@localhost:3306/splitcore'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:%40C82A221S@localhost:3306/splitcore')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY="mykey"
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'mykey')
